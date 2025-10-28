4a765d8b53966c105263c27136459cac7719259f
temp-storage <suportewrsystems@gmail.com>
2025-10-28 19:20:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,8 @@
-const CACHE_NAME = "quiz-biblico-v1";
-const FILES_TO_CACHE = [
-  "/site/bibliaquiz.html",
-  "/site/manifest.json",
-  "/site/imagens/bible-icon.png",
-  "/site/imagens/bible-icon.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', event => {
+  console.log('Service Worker instalado');
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then((res) => res || fetch(e.request))
-  );
+self.addEventListener('fetch', event => {
+  // Permite o site funcionar offline futuramente
+  event.respondWith(fetch(event.request).catch(() => new Response('Você está offline.')));
 });
