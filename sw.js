a617b1ec7b1aba2dfba0be488fa0f42c0db2b812
temp-storage <suportewrsystems@gmail.com>
2025-10-28 16:55:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,12 +8,28 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
 });
